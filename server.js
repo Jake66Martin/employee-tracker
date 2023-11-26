f449337b207f426cc4268fc800129735347f5aed
leviathan <jake66martin@hotmail.com>
2023-11-26 18:27:00 -0500

    created server file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
